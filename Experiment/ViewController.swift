74cd1d24245f34d4e5e55ccbecb99752acd41c4e
cstimler <charles_st@yahoo.com>
2021-04-04 17:45:42 -0700

    1st commit

--- Experiment/ViewController.swift
+++ Experiment/ViewController.swift
@@ -13,6 +13,34 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func experiment(sender: UIButton) {
+        let nextController = UIImagePickerController()
+        present(nextController, animated: true, completion: nil)
+    }
+    
+    @IBAction func imageExperiment() {
+        let image = UIImage()
+        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        present(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func alertExperiment() {
+        let controller = UIAlertController()
+        
+        controller.title = "Test Alert"
+        controller.message = "This is a test"
+        
+        // Alert view doesn't have a built in way to dismiss its view so let's make one:
+        
+        let okAction = UIAlertAction(title: "ok", style: UIAlertAction.Style.default) {
+            action in self.dismiss(animated:true, completion: nil)
+        }
+        
+        controller.addAction(okAction)
+        
+        present(controller, animated: true, completion: nil)
+    }
 
 
 }
